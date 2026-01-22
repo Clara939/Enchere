e860e5743817c67d33196edd48b3f08198641537
chuet2025 <clara.huet2025@campus-eni.fr>
2026-01-22 16:50:48 +0100

    repository utilisateur et test fini

--- src/test/java/fr/eni/enchere/EnchereApplicationTests.java
+++ src/test/java/fr/eni/enchere/EnchereApplicationTests.java
@@ -11,6 +11,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class EnchereApplicationTests {
+
     @Autowired
     UtilisateurRepositorySql utilisateurDAO;
 
@@ -19,47 +20,75 @@ class EnchereApplicationTests {
     @Autowired
     CategorieRepository categorieRepository;
 
+    // TEST DE UTILISATEUR ----------------------------------------------------------------------------------------------------------------------------------------------------
     @Test
-    void  createTables(){
-        jdbcTemplate.execute("IF OBJECT_ID('CATEGORIE', 'U') IS NOT NULL DROP TABLE CATEGORIE");
+    void testCreateUtilisateur() {
+        utilisateurDAO.createUtilisateur(new Utilisateur("user1", "Dupont", "Jean", "0612345678","jean.dupont@example.com",  "12 rue des Lilas", "75001", "Paris", "motdepasse123", 0, true, true));
 
-        jdbcTemplate.execute("create table Categories( " +
-                        "id_categorie bigint identity constraint categorie_pk primary key, " +
-                        "libelle varchar(30) not null unique)");
+        utilisateurDAO.createUtilisateur(new Utilisateur("user2", "Martin", "Claire","0623456789", "claire.martin@example.com",  "45 avenue Victor Hugo", "69002", "Lyon", "password456", 0, false, true));
+
+        utilisateurDAO.createUtilisateur(new Utilisateur("user3", "Bernard", "Luc",  "0634567890","luc.bernard@example.com", "8 boulevard Voltaire", "13001", "Marseille", "secret789", 0, false, false));
     }
 
     @Test
-    void testCreateUtilisateur() {
-        utilisateurDAO.createUtilisateur(new Utilisateur("user1", "Dupont", "Jean", "jean.dupont@example.com", "0612345678", "12 rue des Lilas", "75001", "Paris", "motdepasse123", 0, true, true));
+    void testReadAllUtilisateur() {
+        utilisateurDAO.readAll().forEach(System.out::println);
+    }
 
-        utilisateurDAO.createUtilisateur(new Utilisateur("user2", "Martin", "Claire", "claire.martin@example.com", "0623456789", "45 avenue Victor Hugo", "69002", "Lyon", "password456", 0, false, true));
+    @Test
+    void testReadByIdUtilisateur(){
+        System.out.println(utilisateurDAO.readById(2));
+    }
 
-        utilisateurDAO.createUtilisateur(new Utilisateur("user3", "Bernard", "Luc", "luc.bernard@example.com", "0634567890", "8 boulevard Voltaire", "13001", "Marseille", "secret789", 0, false, false));
-    void createCategorie(){
-categorieRepository.create(new Categorie("Meubles"));
+    @Test
+    void testUpdateUtilisateur(){
+        utilisateurDAO.updateUtilisateur(new Utilisateur(2, "user2", "Marie", "Claire", "0623456789","claire.marie@example.com",  "45 avenue Victor Hugo", "69002", "Lyon", "password456", 0, false, true));
+    }
+
+    @Test
+    void testDeleteUtilisateur(){
+        utilisateurDAO.deleteUtilisateur(3);
+    }
+
+    // TEST DE CATEGORIE -------------------------------------------------------------------------------------------------------------------------------------------------------
+    @Test
+    void createTables() {
+        jdbcTemplate.execute("IF OBJECT_ID('CATEGORIE', 'U') IS NOT NULL DROP TABLE Categories");
+
+        jdbcTemplate.execute("create table Categories( " +
+                "id_categorie bigint identity constraint categorie_pk primary key, " +
+                "libelle varchar(30) not null unique)");
+    }
+
+
+    @Test
+    void createCategorie() {
+        categorieRepository.create(new Categorie("Meubles"));
         categorieRepository.create(new Categorie("Electroménager"));
         categorieRepository.create(new Categorie("Décoration"));
     }
 
     @Test
-    void readAllCategorie(){
+    void readAllCategorie() {
         categorieRepository.readAll().forEach(System.out::println);
     }
 
     @Test
-    void readByIdCategorie(){
+    void readByIdCategorie() {
         System.out.println(categorieRepository.readById(2));
     }
 
     @Test
-    void deleteCategorie(){
-categorieRepository.delete(2);
-categorieRepository.readAll().forEach(System.out::println);
+    void deleteCategorie() {
+        categorieRepository.delete(2);
+        categorieRepository.readAll().forEach(System.out::println);
     }
 
     @Test
-    void updateCategorie(){
+    void updateCategorie() {
         categorieRepository.update(new Categorie(1, "jouets"));
     }
 
+
 }
+
