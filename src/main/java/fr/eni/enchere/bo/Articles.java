ec172726618fad0b68cf423b88c062e02632d29c
chuet2025 <clara.huet2025@campus-eni.fr>
2026-01-22 11:21:26 +0100

    creation bo utilisateur et articles

---
+++ src/main/java/fr/eni/enchere/bo/Articles.java
@@ -0,0 +1,15 @@
+package fr.eni.enchere.bo;
+
+import java.time.LocalDate;
+
+public class Articles {
+    private long id_article;
+    private String nom_article;
+    private String description;
+    private LocalDate date_debut_encheres;
+    private LocalDate date_fin_encheres;
+    private int prix_initial;
+    private int prix_vente;
+    private String etat_vente;
+
+}
