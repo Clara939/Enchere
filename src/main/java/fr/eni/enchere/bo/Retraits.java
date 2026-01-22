5fb2471448674324086a2718099c4938c8d83926
chuet2025 <clara.huet2025@campus-eni.fr>
2026-01-22 11:38:53 +0100

    creation bo enchere

--- src/main/java/fr/eni/enchere/bo/Retraits.java
+++ src/main/java/fr/eni/enchere/bo/Retraits.java
@@ -1,7 +1,6 @@
 package fr.eni.enchere.bo;
 
 public class Retraits {
-    private long id_article;
     private String rue;
     private String code_postal;
     private String ville;
@@ -9,40 +8,17 @@ public class Retraits {
 
     public Retraits() { }
 
-    public Retraits(long id_article, String rue, String code_postal, String ville, long id_retrait) {
-        this.id_article = id_article;
-        this.rue = rue;
+    public Retraits(String code_postal, String rue, String ville, long id_retrait) {
         this.code_postal = code_postal;
+        this.rue = rue;
         this.ville = ville;
         this.id_retrait = id_retrait;
-    }
-
-    public Retraits(String rue, String code_postal, String ville, long id_article) {
-        this.rue = rue;
-        this.code_postal = code_postal;
-        this.ville = ville;
-        this.id_article = id_article;
-    }
-
-    public Retraits(long id_retrait, String ville, String rue, String code_postal) {
-        this.id_retrait = id_retrait;
-        this.ville = ville;
-        this.rue = rue;
-        this.code_postal = code_postal;
     }
 
     public Retraits(String rue, String code_postal, String ville) {
         this.rue = rue;
         this.code_postal = code_postal;
         this.ville = ville;
-    }
-
-    public long getId_article() {
-        return id_article;
-    }
-
-    public void setId_article(long id_article) {
-        this.id_article = id_article;
     }
 
     public String getRue() {
@@ -80,8 +56,7 @@ public class Retraits {
     @Override
     public String toString() {
         return "Retraits{" +
-                "id_article=" + id_article +
-                ", rue='" + rue + '\'' +
+                "rue='" + rue + '\'' +
                 ", code_postal='" + code_postal + '\'' +
                 ", ville='" + ville + '\'' +
                 ", id_retrait=" + id_retrait +
