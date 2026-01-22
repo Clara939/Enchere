1e30a10d3b501620117b4eaa506700052984e6fb
M-Guedon <mathilde.guedon2025@campus-eni.fr>
2026-01-22 12:07:13 +0100

    modif emplacement index.html + creation login.html

---
+++ src/main/java/fr/eni/enchere/controller/converter/LoginController.java
@@ -0,0 +1,12 @@
+package fr.eni.enchere.controller.converter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String displayLogin() {
+        return "login";
+    }
+}
