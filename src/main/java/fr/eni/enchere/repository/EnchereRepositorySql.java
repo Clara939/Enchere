d6f1de6dcc4990a22c55ca841dcd2236287fb678
chuet2025 <clara.huet2025@campus-eni.fr>
2026-01-23 11:14:51 +0100

    repository enchere et test readAll

--- src/main/java/fr/eni/enchere/repository/EnchereRepositorySql.java
+++ src/main/java/fr/eni/enchere/repository/EnchereRepositorySql.java
@@ -3,10 +3,13 @@ package fr.eni.enchere.repository;
 import fr.eni.enchere.bo.Enchere;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class EnchereRepositorySql implements EnchereRepository{
@@ -22,6 +25,21 @@ public class EnchereRepositorySql implements EnchereRepository{
     @Override
     public void createEnchere(Enchere enchere) {
 
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+
+        String sql = "insert into Encheres (date_enchere, montant_enchere, id_encherisseur, article " +
+                    "values (:date_enchere, :montant_enchere, :encherisseur, :article)";
+        MapSqlParameterSource map = new MapSqlParameterSource();
+        map.addValue("date_enchere", enchere.getDate_enchere());
+        map.addValue("montant_enchere", enchere.getMontant_enchere());
+        map.addValue("encherisseur", enchere.getEncherisseur());
+        map.addValue("article", enchere.getArticles());
+
+        namedParameterJdbcTemplate.update(sql, map, keyHolder);
+
+        long id = Objects.requireNonNull(keyHolder.getKey()).longValue();
+
+        enchere.setId_enchere(id);
     }
 
     @Override
--- src/test/java/fr/eni/enchere/EnchereApplicationTests.java
+++ src/test/java/fr/eni/enchere/EnchereApplicationTests.java
@@ -204,4 +204,6 @@ class EnchereApplicationTests {
 
 
 
+
+
 }
