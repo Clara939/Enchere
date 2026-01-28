a3665c3c339129b04f62474766458bee3290f6d6
chuet2025 <clara.huet2025@campus_eni.fr>
2026-01-28 16:29:57 +0100

    modif nouveau article

--- src/main/java/fr/eni/enchere/controller/converter/EnchereController.java
+++ src/main/java/fr/eni/enchere/controller/converter/EnchereController.java
@@ -15,13 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.UUID;
 
 @Controller
 public class EnchereController {
--- src/main/java/fr/eni/enchere/service/RetraitServiceImpl.java
+++ src/main/java/fr/eni/enchere/service/RetraitServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 
 @Service
